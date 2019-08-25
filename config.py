32d53c1997fccaba9fb8692540924da3fc1858eb
unknown <trongnghianguyen1994@gmail.com>
2019-08-25 14:20:20 +0700

    Prod config debug to False

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ class DevConfig(Config):
 
 
 class ProdConfig(Config):
-    DEBUG = True
+    DEBUG = False
     MONGODB_URI = 'mongodb://admin:ZeLGzN1WJ365Y6eq@beagreatcluster-shard-00-00-nzdwh.mongodb.net:27017,beagreatcluster-shard-00-01-nzdwh.mongodb.net:27017,beagreatcluster-shard-00-02-nzdwh.mongodb.net:27017/test?ssl=true&replicaSet=BeagreatCluster-shard-0&authSource=admin&retryWrites=true&w=majority'
     SECRET_KEY = 'A very secret key'
